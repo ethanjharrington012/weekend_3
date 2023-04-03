bb724005ecc160f0cef989723db5441a2862dca4
ethanjharrington012 <ethanjharrington012@gmail.com>
2023-04-02 19:40:31 -0700

    second commit

--- weekend_project_3.py
+++ weekend_project_3.py
@@ -1,84 +1,83 @@
+
+
+
 class Parking_Garage():
     
     def __init__(self,parkingSpaces, name):
         self.tickets_available = parkingSpaces
         self.parkingSpaces = parkingSpaces
-        self.currentTicket = {}
-        self.allowed_parking_time = None
         self.name = name
-# - takeTicket
-# - This should decrease the amount of tickets available by 1
+        self.id_name = {}
+
+
     def takeTicket(self):
-        tickets_needed = int(input("How many tickets do you need?: "))
-        if tickets_needed > self.tickets_available:
-            print("Hey McFly, that's too many tickets! Try less. ")
-        else:            
-            self.tickets_available -= tickets_needed
-            
+        
+        licence_id = input("\nWhat's your license plate number?: ")
+        self.id_name[licence_id] = 1           
+        self.tickets_available -= 1
+        for key, value in self.id_name.items():
+            print(f"\nYou license plate number is {key.upper()} and you purchased {value} tickets")
+        
     def returnTicket(self):
-        tickets_returning = int(input("How many vehicles are leaving?: "))
-        if tickets_returning > self.tickets_available:
-            print(f"Well, that doesn't make sense.  Are you sure that you're returning {tickets_returning}. Try again. ")
-        else:            
-            self.tickets_available += tickets_returning
-            self.checkoutTicket()
+        tickets_returning = input("\nWhats your license plate number: ")
+        for key in self.id_name.keys():
+            if key != tickets_returning:
+                print(f"Try again - there is no plate by the title of {tickets_returning.upper()}")
+            else:
+                self.tickets_available += 1
+                del [tickets_returning]
+                self.checkoutTicket()
 
 
     def showTicketavailable(self): 
-            print(f'{self.tickets_available} tickets available.') 
+            print(f'\n{self.tickets_available} tickets available.') 
       
     def checkoutTicket(self):
-        print(f" Thanks for parking at {self.name}.")
-            
-    def run(self):   
+        while True:
+            payment = input("Please pay your fee of $15.00 (Type: 15.00): ")
+            if payment != "15.00":
+                print("Incorrect amount.")
+                self.checkoutTicket()
+            else:
+                print(f" \nThanks for parking at {self.name}.\nHave a great day!")
+            break
+
+    def showOptions(self):
+        print(f"""
+        Here are your options:
+        [1] Enter {self.name} Garage
+        [2] Exit {self.name} Garage
+        [3] Show Available Tickets
+        [4] Confirm Exit and Print Receipt    
+        """)
+                
+    def run(self):
+           
     
         while True:
-            response = input("What are you doing? Entering/Exiting/Show (tickets): ").lower()
+            
+            quest_name = input("What's your name: ")
+            self.showOptions()
+            
+            response = input(f"\nWelcome to the {self.name} Garage! it is $15.00 a day to park! \n Pick your number option: ")
 
-            if response == 'entering':
+            if response == '1':
                 self.takeTicket()
 
-            elif response == 'exiting':
+            elif response == '2':
                 self.returnTicket()
-            elif response == 'show':
+                
+                
+            elif response == '3':
                 self.showTicketavailable()
-    
-            elif response == 'checkout':
-                self.checkoutTicket()
-    
+            elif response == '4':
+                print("\nHave a great day! ")
                 break
-
             else:
                 print("Invalid entry. Please try again, doofus.")
 
-# Other Ideas:
-"""
-
-- show tickets available
-- possibly add time somehow
-- input variables to enter drivers info: name, vehicle, etc. - stored in dictionary
-- 
-
-"""
-            
-            
-
-            
-# - This should decrease the amount of parkingSpaces available by 1
-# - payForParking
 
 
-# - Display an input that waits for an amount from the user and store it in a variable
-
-# - If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
-# - This should update the "currentTicket" dictionary key "paid" to True
-# -leaveGarage
-# - If the ticket has been paid, display a message of "Thank You, have a nice day"
-# - If the ticket has not been paid, display an input prompt for payment
-# - Once paid, display message "Thank you, have a nice day!"
-# - Update parkingSpaces list to increase by 1 (meaning add to the parkingSpaces list)
-
-
-ethans_Garage = Parking_Garage(100, "Ethans ass garage")
+ethans_Garage = Parking_Garage(100, "E, B, L's Cool-Ass")
             
 ethans_Garage.run()
